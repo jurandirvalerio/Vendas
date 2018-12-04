e460ab882ad6ed52e3204feca3ec2708c1fb7d86
Jurandir Valerio <jjunior@prima.com.br>
2018-12-04 14:44:52 -0200

    Renomeando classe.

---
+++ AcessoDados.Modules/VendaContextModule.cs
@@ -0,0 +1,18 @@
+﻿using AcessoDados.Contratos;
+using AcessoDados.Implementacoes;
+using Ninject.Modules;
+
+namespace AcessoDados.Modules
+{
+	public class VendaContextModule : NinjectModule
+	{
+		#region Métodos
+
+		public override void Load()
+		{
+			Bind<IVendasContext>().To<VendasContext>();
+		}
+
+		#endregion
+	}
+}
